c8d485c20c3cfd0dcbd385df1eaa15e60186be26
StephenSchuch <Stephen.Schuch@gmail.com>
2016-01-11 21:12:07 -0600

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,13 @@
+hpc <- read.table("hpc.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subhpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
+datehpc <- strptime(paste(subhpc$Date, subhpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+numhpc <- as.numeric(subhpc$Global_active_power)
+subMetering1 <- as.numeric(subhpc$Sub_metering_1)
+subMetering2 <- as.numeric(subhpc$Sub_metering_2)
+subMetering3 <- as.numeric(subhpc$Sub_metering_3)
+png("plot3.png", width=480, height=480)
+plot(datehpc, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datehpc, subMetering2, type="l", col="red")
+lines(datehpc, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off() 
