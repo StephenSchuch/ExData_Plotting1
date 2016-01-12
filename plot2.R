5a48cec76be30383360d43bb5d89c7d941205beb
StephenSchuch <Stephen.Schuch@gmail.com>
2016-01-11 21:10:35 -0600

    Delete plot2.R

--- plot2.R
+++
@@ -1,7 +0,0 @@
-hpc <- read.table("hpc.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subhpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
-datehpc <- strptime(paste(subhpc$Date, subhpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-numhpc <- as.numeric(subhpc$Global_active_power)
-png("plot2.png", width=480, height=480)
-plot(datehpc, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
-dev.off() 
